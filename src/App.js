317cdbbe1e363fb0ac5d35647f0a8999eaabe81d
rafael346 <sousa_raf@hotmail.com>
2021-05-09 17:21:33 -0300

    Add: Login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Detail from './components/Detail';
+import Login from './components/Login';
 
 import {
   BrowserRouter as Router,
@@ -20,7 +21,9 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/detail">
             <Detail />
           </Route>
